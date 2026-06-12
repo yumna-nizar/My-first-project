804f29690a596e5339b2d8892581d1bac7beea75
yumna-nizar <nizarrasheeda270@gmail.com>
2026-06-12 22:20:22 +0530

    the AddStudent Component designed with css

--- Frontend/src/Components/AddStudent.jsx
+++ Frontend/src/Components/AddStudent.jsx
@@ -1,14 +1,36 @@
-function AddStudent()
-{
-    function submitSudent()
-    {}
+import { useState } from "react";
+import "../CompStyles/addstudent.css";
 
-return (<>
-<h1>Student Adding Form</h1>
-<input type="text" onChange={(e)=>{e.target.value}}></input>
-<input type="text" onChange={(e)=>{e.target.value}}></input>
-<input type="text" onChange={(e)=>{e.target.value}}></input>
-<button>Submit</button>
-</>)
+function AddStudent() {
+  const [studentname, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [std, setstd] = useState("");
+
+  function submitSudent() {
+    console.log(`${studentname} ${email} ${std}`);
+  }
+
+  return (
+    <div className="addstudent-container">
+      <h1>Student Adding Form</h1>
+      <div className="form-group">
+        <label>Student name:</label>
+        <input type="text" onChange={(e) => setname(e.target.value)}></input>
+      </div>
+      <div className="form-group">
+        <label>Email:</label>
+        <input type="text" onChange={(e) => setemail(e.target.value)}></input>
+      </div>
+      <div className="form-group">
+        <label>Class:</label>
+        <input type="text" onChange={(e) => setstd(e.target.value)}></input>
+      </div>
+      <div className="button-container">
+        <button className="submit-btn" onClick={submitSudent}>
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
-export  default AddStudent;
+export default AddStudent;
