e375b1e29808d63bc49e85a18eea6c7c1a7d8bb5
yumna-nizar <nizarrasheeda270@gmail.com>
2026-06-16 14:33:19 +0530

    designed viewstudent page(added table to view)

--- Frontend/src/Components/ViewStudent.jsx
+++ Frontend/src/Components/ViewStudent.jsx
@@ -1,7 +1,22 @@
-function ViewStudent()
-{
-    return (<>
-    <h1>Student viewing page</h1>
-    </>)
+import "../CompStyles/viewstudent.css";
+
+function ViewStudent() {
+  return (
+    <div className="viewstudent-container">
+      <h1>Student viewing page</h1>
+      <table className="student-table">
+        <thead>
+          <tr>
+            <th>Student name</th>
+            <th>Age</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr></tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
 export default ViewStudent;
